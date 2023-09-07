779d3514c2a6af8009e6f6aff203677f90aea200
Nedas Cerneckis <nedcerneckis@gmail.com>
2023-09-07 21:28:38 +0100

    Initial commit

---
+++ src/test/java/com/ned/mockingbird/MockingbirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ned.mockingbird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockingbirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
